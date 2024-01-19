9645849f519a792e3626a87a286fee49867a222d
Ultimativ93 <norman.jauchen@gmail.com>
2024-01-19 20:56:28 -0300

    Neue Struktur, React-Flow eingearbeitet, gerade dabei Custom Knoten zu erstellen mit dem MultipleChoice Knoten, Datenstruktur für den MuChoi schon ausgearbeitet, ebenso für Start und Bridge. Todos: Audio upload möglichkeit einarbeiten, gucken ob wie wir das wo dann abspeichern

--- server/src/user/routes.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/InteraktivEditor", (req, res) => {
-    res.redirect('http://localhost:3000');
-});
-
-export default router;
